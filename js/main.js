43f740d783456f6de750a81046cd7d84926884bd
M-Zayn <59746872+M-Zayn@users.noreply.github.com>
2020-05-31 02:32:04 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,193 @@
+
+$(window).on("load", function () {
+  $('body,html').scrollTop(0)
+  $(".pre-loader").fadeOut(500, function () {
+      $(this).remove();
+      $('html, body').css("overflow-y", "visible");
+
+
+  });
+});
+
+  new WOW().init();
+  
+  
+$(function() {
+
+
+  
+  
+  // $('a[data-scroll]').on("click",function (e){
+  //   e.preventDefault();    
+    
+  //   $("html, body").animate({
+  //     scrollTop: $($(this).attr("data-scroll")).offset().top
+  //   }, 1000);
+  // });
+
+
+
+  // $(".selector .select li a").on("shown.bs.tab", function(e) {
+  //   let $owl = $(".tab-content .owl-carousel");
+  //   $owl.trigger("refresh.owl.carousel");
+  // });
+
+
+    $(".nav-list .list li:first-child").on("click", function() {
+      $(".nav-list .sub-list").slideToggle();
+    });
+  
+    
+    $('.header .owl-carousel').owlCarousel({
+      rtl: document.dir == 'rtl' ? true : false,
+      autoplay:true,
+      loop:true,
+      margin:1,
+      dots:true,
+      nav:true,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:1
+          },
+          1000:{
+              items:1
+          }
+      },
+      navText: ["<span class='fa fa-chevron-right'></span>","<span class='fa fa-chevron-left'></span>"]
+    });
+
+
+
+
+    $('.sp-offers .owl-carousel').owlCarousel({
+      rtl: document.dir == 'rtl' ? true : false,
+      autoplay:true,
+      loop:true,
+      margin:0,
+      dots:true,
+      nav:true,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:2
+          },
+          1000:{
+              items:4,
+          }
+      },
+    });
+
+
+    $('.co-workers .owl-carousel').owlCarousel({
+      rtl: document.dir == 'rtl' ? true : false,
+      autoplay:true,
+      loop:true,
+      margin:30,
+      dots:true,
+      nav:true,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:2
+          },
+          600:{
+              items:4
+          },
+          1000:{
+              items:9,
+          }
+      },
+    });
+
+
+    
+
+    
+    
+
+
+    
+    
+  
+
+  // menu - search
+
+
+
+
+    $(".buy-options .search-icon").on("click", function () {
+      $(".search-container").fadeIn();
+      $('html, body').css("overflow-y", "hidden");
+    });
+    $(".search-container, .search-container .close").on("click", function () {
+      $(".search-container").fadeOut();
+      $('html, body').css("overflow-y", "visible");
+
+    });
+    $(".zx-search").on("click", function(e) {
+      e.stopPropagation();
+    });
+
+
+
+    
+
+
+
+
+    $(".nav-list .buy-options .menu-icon").on("click", function () {
+      $(".fixed-menu").fadeIn();
+      $(".fixed-menu .menu").addClass("show");
+      $(".fixed-menu .main-list").addClass("show");
+      $('html, body').css("overflow-y", "hidden");
+    });
+    $(".fixed-menu, .fixed-menu .menu-header .close").on("click", function () {
+      $(".fixed-menu").fadeOut();
+      $(".fixed-menu .menu").removeClass("show");
+      $(".fixed-menu .main-list").removeClass("show");
+      $('html, body').css("overflow-y", "visible");
+    });
+    $(".fixed-menu .menu").on("click", function(e) {
+      e.stopPropagation();
+    });
+
+
+
+    $(".fixed-menu .main-list .slide").on("click", function() {
+      $(this).toggleClass("active");
+      $(this).children("ul").slideToggle();
+      });
+    $(".fixed-menu .sub-list .slide-two").on("click", function(e) {
+      e.stopPropagation();
+    });
+    $(".fixed-menu .sub-list .slide-two").on("click", function() {
+      $(this).toggleClass("active");
+      $(this).children("ul").slideToggle();
+    });
+
+
+     if ($(window).width() < 992) {
+       $(".footer .list-header").on("click", function() {
+         $(this).toggleClass("active");
+         $(this).siblings("ul").slideToggle();
+       });
+     
+      } else {
+        
+      }
+
+    
+
+
+
+
+
+  
+});
